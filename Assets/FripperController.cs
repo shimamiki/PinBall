18ddc737b666ccf31f5baf4f1e3d6359f5f3fa19
shimamiki <tamifull@jcom.zaq.ne.jp>
2018-07-22 16:27:56 +0900

    PinBall 初めてのコミット
    得点が表示されるようにした。

---
+++ Assets/FripperController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+	//HingiJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	//初期の傾き
+	private float defaultAngle = 20;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+
+	// Use this for initialization
+	void Start () {
+		//HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//左矢印キーを押した時左フリッパーを動かす
+		if (Input.GetKeyDown(KeyCode.LeftArrow)&& tag =="LeftFripperTag"){
+			SetAngle (this.flickAngle);
+		}
+		//右矢印キーを押した時右フリッパーを動かす
+		if(Input.GetKeyDown(KeyCode.RightArrow)&& tag =="RightFripperTag"){
+			SetAngle (this.flickAngle);
+		}
+
+		//矢印キーが離された時フリッパーを元に戻す
+		if(Input.GetKeyUp(KeyCode.LeftArrow) && tag =="LeftFripperTag"){
+			SetAngle (this.defaultAngle);
+		}
+		if(Input.GetKeyUp(KeyCode.RightArrow) && tag =="RightFripperTag"){
+			SetAngle (this.defaultAngle);
+		}
+
+
+
+		//スマホタップ対応----------------------------------
+
+
+//		Touch[] touches = Input.touches;
+
+//		foreach (var touch in touches) {
+//			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || TouchPhase.Stationary) {
+//				if (touch.position.x > Screen.width / 2) {
+					//右側がタップされたら
+//					SetAngle (this.flickAngle);
+//				} else{
+					//左側がタップされたら
+//					SetAngle (this.flickAngle);
+//				}
+
+//			}
+//		}
+
+		
+	}
+
+
+	//フリッパーの傾きを設定
+	public void SetAngle(float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+
+
+}
---
+++ Assets/Scorecontroller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scorecontroller : MonoBehaviour {
+
+	//スコアを表示するテキスト
+	private GameObject scoreText;
+
+	//現在の得点
+	int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//シーン中のTextを取得する
+		this.scoreText = GameObject.Find("ScoreText");
+
+		//現在の得点を表示
+		this.scoreText.GetComponent<Text>().text = score.ToString();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//ScoreTextに現在の得点を表示する
+		this.scoreText.GetComponent<Text>().text = score.ToString();
+	
+		
+	}
+
+
+	//それぞれのオブジェクトにぶつかったとき、それぞれの得点を追加する
+	void OnCollisionEnter (Collision other){
+		
+		if (other.gameObject.tag == "SmallStarTag") {
+			this.score += 5;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			this.score += 50;
+		} else if (other.gameObject.tag == "smallCloudTag" || other.gameObject.tag == "LargeCloudTag") {
+			this.score += 10;
+		}
+	
+	}
+
+}
